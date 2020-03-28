60db5f8a78af0884028e72dad966a77b5fbe7af0
pxu <19125258@bjtu.edu.cn>
2020-03-28 21:38:19 +0800

    NIO learn

---
+++ src/main/java/com/xp/NIO_Learn/AClient.java
@@ -0,0 +1,12 @@
+package com.xp.NIO_Learn;
+
+import java.io.IOException;
+
+public class AClient {
+
+    public static void main(String[] args)
+            throws IOException {
+        new NioClient().start("AClient");
+    }
+
+}
---
+++ src/main/java/com/xp/NIO_Learn/CClient.java
@@ -0,0 +1,12 @@
+package com.xp.NIO_Learn;
+
+import java.io.IOException;
+
+public class CClient {
+
+    public static void main(String[] args)
+            throws IOException {
+        new NioClient().start("CClient");
+    }
+
+}
